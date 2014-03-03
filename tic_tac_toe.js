69b6d9445b8b944676d250907237dbb6879f67e2
John Moon <johnmoonyy@gmail.com>
2014-03-02 19:31:01 -0500

    AI built for when computer goes first

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -4,13 +4,396 @@ var third_row = ['+','+','+'];
 
 var player1_wins = 'ooo';
 var player2_wins = 'xxx';
+var state = [];
 
 var players = ['o','x'];
-var counter = 0;
+
+var first_move = 'who'
+
+var counter = Math.floor((Math.random()*2)+1);
+var counter = Math.floor((Math.random()*2)+1);
+
+
+function pageLoad() {
+  if(counter == 3){
+    //computer 2nd move
+    if(first_move == 'computer') {
+      if($("#row3 span2").text() == '+') {
+        counter++;
+        return $("#row3 span2")[0].innerHTML = 'x';
+      } else {
+        counter++;
+        return $("#row3 span0")[0].innerHTML = "x";
+      };
+    } else {
+    //computer 1st move
+      if($("#row2 span1").text() == '+') {
+        counter++;
+        return $("#row2 span1")[0].innerHTML = "x";
+      } else {
+        counter++;
+        return $("#row1 span0")[0].innerHTML = "x";
+      };
+    };
+  } else if(counter == 5) {
+//3rd move by computer. Check if there are two player pieces that are
+//placed together. Block the winning slot. If not place piece in the
+//corner.
+    if(first_move == 'computer'){
+      i = 0
+      state.splice(0, 3);
+      while(i < 4) {
+        for(n = 0; n < 3; n++) {
+          //checking rows
+          if(state[i] == "+oo") {
+            var r = i+1
+            counter++
+            return $('#row' +r+ ' span0')[0].innerHTML = 'x';
+          }
+          if(state[i] == "o+o") {
+            var r = i+1
+            counter++
+            return $('#row' +r+ ' span1')[0].innerHTML = 'x';
+          }
+          if(state[i] == "oo+") {
+            var r = i+1
+            counter++
+            return $('#row' +r+ ' span2')[0].innerHTML = 'x';
+          }
+          //checking columns
+          if([state[0][i] == "o" && state[1][i] =='o' && state[2][i]] == '+') {
+            counter++
+            return $('#row3'+ ' span'+i)[0].innerHTML = 'x';
+          }
+          if([state[0][i] == "o" && state[1][i] =='+' && state[2][i]] == 'o') {
+            counter++
+            return $('#row2'+ ' span'+i)[0].innerHTML = 'x';
+          }
+          if([state[0][i] == "+" && state[1][i] =='o' && state[2][i]] == 'o') {
+            counter++
+            return $('#row1'+ ' span'+i)[0].innerHTML = 'x';
+          }
+          //top left diagonal
+          if([state[0][0] == "+" && state[1][1] =='o' && state[2][2]] == 'o') {
+            counter++
+            return $('#row1' + ' span0')[0].innerHTML = 'x';
+          }
+          if([state[0][0] == "o" && state[1][1] =='+' && state[2][2]] == 'o') {
+            counter++
+            return $('#row2'+ ' span1')[0].innerHTML = 'x';
+          }
+          if([state[0][0] == "o" && state[1][1] =='o' && state[2][2]] == '+') {
+            counter++
+            return $('#row3'+ ' span2')[0].innerHTML = 'x';
+          }
+          //top right diagonal
+          if([state[2][0] == "+" && state[1][1] =='o' && state[0][2]] == 'o') {
+            counter++
+            return $('#row3' + ' span0')[0].innerHTML = 'x';
+          }
+          if([state[2][0] == "o" && state[1][1] =='+' && state[0][2]] == 'o') {
+            counter++
+            return $('#row2'+ ' span1')[0].innerHTML = 'x';
+          }
+          if([state[2][0] == "o" && state[1][1] =='o' && state[0][2]] == '+') {
+            counter++
+            return $('#row1'+ ' span2')[0].innerHTML = 'x';
+          }
+        };
+        i++;
+      };
+    } else {
+//computers 2nd move. Check for player pieces that are together. Then block. If not connect
+//own pieces for a chance to win.
+      state.splice(0, 3);
+    };
+  } else if(counter == 7) {
+    if(first_move == 'computer') {
+     i = 0
+      state.splice(0, 3);
+      while(i < 4) {
+        for(n = 0; n < 3; n++) {
+         //checking rows if computer can win
+          if(state[i] == "+xx") {
+            var r = i+1
+            return $('#row' +r+ ' span0')[0].innerHTML = 'x';
+          }
+          if(state[i] == "x+x") {
+            var r = i+1
+            return $('#row' +r+ ' span1')[0].innerHTML = 'x';
+          }
+          if(state[i] == "xx+") {
+            var r = i+1
+            return $('#row' +r+ ' span2')[0].innerHTML = 'x';
+          };
+        //checking columns if computer can win
+          if([state[0][i] == "x" && state[1][i] =='x' && state[2][i]] == '+') {
+            counter++
+            return $('#row3'+ ' span'+i)[0].innerHTML = 'x';
+          }
+          if([state[0][i] == "x" && state[1][i] =='+' && state[2][i]] == 'x') {
+            counter++
+            return $('#row2'+ ' span'+i)[0].innerHTML = 'x';
+          }
+          if([state[0][i] == "+" && state[1][i] =='x' && state[2][i]] == 'x') {
+            counter++
+            return $('#row1'+ ' span'+i)[0].innerHTML = 'x';
+          }
+          //checking top left diagonal if computer can win
+          if([state[0][0] == "+" && state[1][1] =='x' && state[2][2]] == 'x') {
+            counter++
+            return $('#row1' + ' span0')[0].innerHTML = 'x';
+          }
+          if([state[0][0] == "x" && state[1][1] =='+' && state[2][2]] == 'x') {
+            counter++
+            return $('#row2'+ ' span1')[0].innerHTML = 'x';
+          }
+          if([state[0][0] == "x" && state[1][1] =='x' && state[2][2]] == '+') {
+            counter++
+            return $('#row3'+ ' span2')[0].innerHTML = 'x';
+          }
+
+          //checking top right diagonal if computer can win
+          if([state[2][0] == "+" && state[1][1] =='x' && state[0][2]] == 'x') {
+            counter++
+            return $('#row3' + ' span0')[0].innerHTML = 'x';
+          }
+          if([state[2][0] == "x" && state[1][1] =='+' && state[0][2]] == 'x') {
+            counter++
+            return $('#row2'+ ' span1')[0].innerHTML = 'x';
+          }
+          if([state[2][0] == "x" && state[1][1] =='x' && state[0][2]] == '+') {
+            counter++
+            return $('#row1'+ ' span2')[0].innerHTML = 'x';
+          }
+        };
+        i++;
+      };
+
+      i = 0
+      while(i < 4) {
+        for(n = 0; n < 3; n++) {
+          //top right diagonal to prevent player from winning
+          if([state[2][0] == "+" && state[1][1] =='o' && state[0][2]] == 'o') {
+            counter++
+            return $('#row3' + ' span0')[0].innerHTML = 'x';
+          }
+          if([state[2][0] == "o" && state[1][1] =='+' && state[0][2]] == 'o') {
+            counter++
+            return $('#row2'+ ' span1')[0].innerHTML = 'x';
+          }
+          if([state[2][0] == "o" && state[1][1] =='o' && state[0][2]] == '+') {
+            counter++
+            return $('#row1'+ ' span2')[0].innerHTML = 'x';
+          }
+
+          //checking rows to prevent player form winning
+          if(state[i] == "+oo") {
+            var r = i+1
+            return $('#row' +r+ ' span0');
+          }
+          if(state[i] == "o+o") {
+            var r = i+1
+            return $('#row' +r+ ' span1');
+          }
+          if(state[i] == "oo+") {
+            var r = i+1
+            return $('#row' +r+ ' span2')[0].innerHTML = 'x';
+          }
+
+          //checking columns to prevent player from winning
+          if([state[0][i] == "o" && state[1][i] =='o' && state[2][i]] == '+') {
+            counter++
+            return $('#row3'+ ' span'+i)[0].innerHTML = 'x';
+          }
+          if([state[0][i] == "o" && state[1][i] =='+' && state[2][i]] == 'o') {
+            counter++
+            return $('#row2'+ ' span'+i)[0].innerHTML = 'x';
+          }
+          if([state[0][i] == "+" && state[1][i] =='o' && state[2][i]] == 'o') {
+            counter++
+            return $('#row1'+ ' span'+i)[0].innerHTML = 'x';
+          }
+
+          //top left diagonal to prevent player from winning
+          if([state[0][0] == "+" && state[1][1] =='o' && state[2][2]] == 'o') {
+            counter++
+            return $('#row1' + ' span0')[0].innerHTML = 'x';
+          }
+          if([state[0][0] == "o" && state[1][1] =='+' && state[2][2]] == 'o') {
+            counter++
+            return $('#row2'+ ' span1')[0].innerHTML = 'x';
+          }
+          if([state[0][0] == "o" && state[1][1] =='o' && state[2][2]] == '+') {
+            counter++
+            return $('#row3'+ ' span2')[0].innerHTML = 'x';
+          }
+        };
+        i++;
+      };
+    } else {
+      //computers last move after going 2nd
+    };
+  } else if(counter == 9) {
+      //computers last move after going 1st
+    if(first_move == 'computer') {
+      i = 0
+      state.splice(0, 3);
+      while(i < 4) {
+        for(n = 0; n < 3; n++) {
+         //checking rows if computer can win
+          if(state[i] == "+xx") {
+            var r = i+1
+            return $('#row' +r+ ' span0')[0].innerHTML = 'x';
+          }
+          if(state[i] == "x+x") {
+            var r = i+1
+            return $('#row' +r+ ' span1')[0].innerHTML = 'x';
+          }
+          if(state[i] == "xx+") {
+            var r = i+1
+            return $('#row' +r+ ' span2')[0].innerHTML = 'x';
+          }
+
+          //checking columns if computer can win
+          if([state[0][i] == "x" && state[1][i] =='x' && state[2][i]] == '+') {
+            counter++
+            return $('#row3'+ ' span'+i)[0].innerHTML = 'x';
+          }
+          if([state[0][i] == "x" && state[1][i] =='+' && state[2][i]] == 'x') {
+            counter++
+            return $('#row2'+ ' span'+i)[0].innerHTML = 'x';
+          }
+          if([state[0][i] == "+" && state[1][i] =='x' && state[2][i]] == 'x') {
+            counter++
+            return $('#row1'+ ' span'+i)[0].innerHTML = 'x';
+          }
+
+          //checking top left diagonal if computer can win
+          if([state[0][0] == "+" && state[1][1] =='x' && state[2][2]] == 'x') {
+            counter++
+            return $('#row1' + ' span0')[0].innerHTML = 'x';
+          }
+          if([state[0][0] == "x" && state[1][1] =='+' && state[2][2]] == 'x') {
+            counter++
+            return $('#row2'+ ' span1')[0].innerHTML = 'x';
+          }
+          if([state[0][0] == "x" && state[1][1] =='x' && state[2][2]] == '+') {
+            counter++
+            return $('#row3'+ ' span2')[0].innerHTML = 'x';
+          }
+
+          //checking top right diagonal if computer can win
+          if([state[2][0] == "+" && state[1][1] =='x' && state[0][2]] == 'x') {
+            counter++
+            return $('#row3' + ' span0')[0].innerHTML = 'x';
+          }
+          if([state[2][0] == "x" && state[1][1] =='+' && state[0][2]] == 'x') {
+            counter++
+            return $('#row2'+ ' span1')[0].innerHTML = 'x';
+          }
+          if([state[2][0] == "x" && state[1][1] =='x' && state[0][2]] == '+') {
+            counter++
+            return $('#row1'+ ' span2')[0].innerHTML = 'x';
+          }
+
+          //checking top right diagonal if computer can win
+          if([state[2][0] == "+" && state[1][1] =='x' && state[0][2]] == 'x') {
+            counter++
+            return $('#row3' + ' span0')[0].innerHTML = 'x';
+          }
+          if([state[2][0] == "x" && state[1][1] =='+' && state[0][2]] == 'x') {
+            counter++
+            return $('#row2'+ ' span1')[0].innerHTML = 'x';
+          }
+          if([state[2][0] == "x" && state[1][1] =='x' && state[0][2]] == '+') {
+            counter++
+            return $('#row1'+ ' span2')[0].innerHTML = 'x';
+          }
+        };
+        i++
+      };
+
+      i = 0
+      while(i < 4) {
+        for(n = 0; n < 3; n++) {
+          //checking rows to prevent player form winning
+          if(state[i] == "+oo") {
+            var r = i+1
+            return $('#row' +r+ ' span0')[0].innerHTML = 'x';
+          }
+          if(state[i] == "o+o") {
+            var r = i+1
+            return $('#row' +r+ ' span1')[0].innerHTML = 'x';
+          }
+          if(state[i] == "oo+") {
+            var r = i+1
+            return $('#row' +r+ ' span2')[0].innerHTML = 'x';
+          }
+
+          //checking columns to prevent player from winning
+          if([state[0][i] == "o" && state[1][i] =='o' && state[2][i]] == '+') {
+            counter++
+            return $('#row3'+ ' span'+i)[0].innerHTML = 'x';
+          }
+          if([state[0][i] == "o" && state[1][i] =='+' && state[2][i]] == 'o') {
+            counter++
+            return $('#row2'+ ' span'+i)[0].innerHTML = 'x';
+          }
+          if([state[0][i] == "+" && state[1][i] =='o' && state[2][i]] == 'o') {
+            counter++
+            return $('#row1'+ ' span'+i)[0].innerHTML = 'x';
+          }
+
+          //top left diagonal to prevent player from winning
+          if([state[0][0] == "+" && state[1][1] =='o' && state[2][2]] == 'o') {
+            counter++
+            return $('#row1' + ' span0')[0].innerHTML = 'x';
+          }
+          if([state[0][0] == "o" && state[1][1] =='+' && state[2][2]] == 'o') {
+            counter++
+            return $('#row2'+ ' span1')[0].innerHTML = 'x';
+          }
+          if([state[0][0] == "o" && state[1][1] =='o' && state[2][2]] == '+') {
+            counter++
+            return $('#row3'+ ' span2')[0].innerHTML = 'x';
+          }
+
+          //top right diagonal to prevent player from winning
+          if([state[2][0] == "+" && state[1][1] =='o' && state[0][2]] == 'o') {
+            counter++
+            return $('#row3' + ' span0')[0].innerHTML = 'x';
+          }
+          if([state[2][0] == "o" && state[1][1] =='+' && state[0][2]] == 'o') {
+            counter++
+            return $('#row2'+ ' span1')[0].innerHTML = 'x';
+          }
+          if([state[2][0] == "o" && state[1][1] =='o' && state[0][2]] == '+') {
+            counter++
+            return $('#row1'+ ' span2')[0].innerHTML = 'x';
+          }
+        };
+        i++;
+      };
+    };
+  };
+};
+
+function firstMove() {
+  if(players[counter % 2] == 'x'){
+    alert("Computer goes first");
+    first_move = 'computer'
+    $("#row1 span0")[0].innerHTML = "x";
+    counter++;
+  } else {
+    first_move = 'player'
+    alert("Player goes first");
+  };
+}
 
 function countClick() {
   counter++;
-}
+};
 
 //check_board
 function checkBoard() {
@@ -21,14 +404,14 @@ function checkBoard() {
       if($("#row" + i).text()[n] == "+") {
         space_left++;
       }
-    }
+    };
     i++;
-  }
+  };
   if(space_left == 0) {
     if(confirm("Tie game! Nobody wins. Play again?")) {
       return location.reload();
     };
-  }
+  };
 }
 
 //check_row
@@ -39,7 +422,7 @@ function checkRow() {
   } else if($("#row1").text() == player2_wins ||
             $("#row2").text() == player2_wins ||
             $("#row3").text() == player2_wins) {
-    alert("Player 2 wins");
+    alert("Computer wins");
     return location.reload();
   };
 }
@@ -53,7 +436,7 @@ function checkColumn() {
       alert("Player 1 wins");
       return location.reload();
     } else if(column == player2_wins) {
-      alert("Player 2 wins");
+      alert("Computer wins");
       return location.reload();
     };
     i++
@@ -68,7 +451,7 @@ function checkDiagonal() {
     alert("Player 1 wins");
     return location.reload();
   } else if(diagonal_right == player2_wins || diagonal_left == player2_wins) {
-    alert("Player 2 wins");
+    alert("Computer wins");
     return location.reload();
   }
 }
@@ -102,6 +485,9 @@ onload = function()
     $("#row1 span0").click(function(){
       if ($("#row1 span0")[0].innerHTML == "+") {
         $("#row1 span0")[0].innerHTML = players[counter % 2];
+        state.push($("#row1").text());
+        state.push($("#row2").text());
+        return state.push($("#row3").text());
       } else {
       counter = counter - 1;
       };
@@ -111,6 +497,9 @@ onload = function()
     $("#row1 span1").click(function(){
       if ($("#row1 span1")[0].innerHTML == "+") {
         $("#row1 span1")[0].innerHTML = players[counter % 2];
+        state.push($("#row1").text());
+        state.push($("#row2").text());
+        return state.push($("#row3").text());
       } else {
       counter = counter - 1;
       };
@@ -120,6 +509,9 @@ onload = function()
     $("#row1 span2").click(function(){
       if ($("#row1 span2")[0].innerHTML == "+") {
         $("#row1 span2")[0].innerHTML = players[counter % 2];
+        state.push($("#row1").text());
+        state.push($("#row2").text());
+        return state.push($("#row3").text());
       } else {
       counter = counter - 1;
       };
@@ -131,6 +523,9 @@ onload = function()
     $("#row2 span0").click(function(){
       if ($("#row2 span0")[0].innerHTML == "+") {
         $("#row2 span0")[0].innerHTML = players[counter % 2];
+        state.push($("#row1").text());
+        state.push($("#row2").text());
+        return state.push($("#row3").text());
       } else {
         counter = counter - 1;
       };
@@ -140,6 +535,9 @@ onload = function()
     $("#row2 span1").click(function(){
       if ($("#row2 span1")[0].innerHTML == "+") {
         $("#row2 span1")[0].innerHTML = players[counter % 2];
+        state.push($("#row1").text());
+        state.push($("#row2").text());
+        return state.push($("#row3").text());
       } else {
       counter = counter - 1;
       };
@@ -149,6 +547,9 @@ onload = function()
     $("#row2 span2").click(function(){
       if ($("#row2 span2")[0].innerHTML == "+") {
         $("#row2 span2")[0].innerHTML = players[counter % 2];
+        state.push($("#row1").text());
+        state.push($("#row2").text());
+        return state.push($("#row3").text());
       } else {
       counter = counter - 1;
       };
@@ -160,6 +561,9 @@ onload = function()
     $("#row3 span0").click(function(){
       if ($("#row3 span0")[0].innerHTML == "+") {
         $("#row3 span0")[0].innerHTML = players[counter % 2];
+        state.push($("#row1").text());
+        state.push($("#row2").text());
+        return state.push($("#row3").text());
       } else {
         counter = counter - 1;
       };
@@ -169,6 +573,9 @@ onload = function()
     $("#row3 span1").click(function(){
       if ($("#row3 span1")[0].innerHTML == "+") {
         $("#row3 span1")[0].innerHTML = players[counter % 2];
+        state.push($("#row1").text());
+        state.push($("#row2").text());
+        return state.push($("#row3").text());
       } else {
         counter = counter - 1;
       };
@@ -178,10 +585,12 @@ onload = function()
     $("#row3 span2").click(function(){
       if ($("#row3 span2")[0].innerHTML == "+") {
         $("#row3 span2")[0].innerHTML = players[counter % 2];
+        state.push($("#row1").text());
+        state.push($("#row2").text());
+        return state.push($("#row3").text());
       } else {
         counter = counter - 1;
       };
     });
   });
 }
-
