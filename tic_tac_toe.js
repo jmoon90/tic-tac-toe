a31326fa6c46f3332a276d4867ab07b85b0740c0
John Moon <johnmoonyy@gmail.com>
2014-03-08 16:10:06 -0500

    omw

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -276,23 +276,28 @@ onload = function()
   var row_2 = document.getElementById('row2');
   var row_3 = document.getElementById('row3');
 
-  for (var i in first_row) {
-    var span = document.createElement('span'+i);
-    span.innerHTML = first_row[i];
-    row_1.appendChild(span);
+  var rows = [row_1, row_2, row_3];
+  var board = [first_row, second_row, third_row];
+
+  for(var row in rows) {
+    for(i = 0; i < 3; i++) {
+debugger
+      var span = $('span').addClass('square span-'+i)(board[Number(row)+1][i]);
+      .append(span);
+    };
   }
 
-  for (var i in second_row) {
-    var span = document.createElement('span'+i);
-    span.innerHTML = second_row[i];
-    row_2.appendChild(span);
-  }
-
-  for (var i in third_row) {
-    var span = document.createElement('span'+i);
-    span.innerHTML = third_row[i];
-    row_3.appendChild(span);
-  }
+//  for (var i in second_row) {
+//    var span = document.createElement('span'+i);
+//    span.innerHTML = second_row[i];
+//    row_2.appendChild(span);
+//  }
+//
+//  for (var i in third_row) {
+//    var span = document.createElement('span'+i);
+//    span.innerHTML = third_row[i];
+//    row_3.appendChild(span);
+//  }
 
 //first row
   $(document).ready(function(){
@@ -326,65 +331,4 @@ onload = function()
     });
   });
 //second row
-  $(document).ready(function(){
-    $("#row2 span0").click(function(){
-      if ($("#row2 span0")[0].innerHTML == "+") {
-        $("#row2 span0")[0].innerHTML = players[counter % 2];
-        push_to_state();
-      } else {
-        counter = counter - 1;
-      };
-    });
-  });
-  $(document).ready(function(){
-    $("#row2 span1").click(function(){
-      if ($("#row2 span1")[0].innerHTML == "+") {
-        $("#row2 span1")[0].innerHTML = players[counter % 2];
-        push_to_state();
-      } else {
-      counter = counter - 1;
-      };
-    });
-  });
-  $(document).ready(function(){
-    $("#row2 span2").click(function(){
-      if ($("#row2 span2")[0].innerHTML == "+") {
-        $("#row2 span2")[0].innerHTML = players[counter % 2];
-        push_to_state();
-      } else {
-      counter = counter - 1;
-      };
-    });
-  });
-//third row
-  $(document).ready(function(){
-    $("#row3 span0").click(function(){
-      if ($("#row3 span0")[0].innerHTML == "+") {
-        $("#row3 span0")[0].innerHTML = players[counter % 2];
-        push_to_state();
-      } else {
-        counter = counter - 1;
-      };
-    });
-  });
-  $(document).ready(function(){
-    $("#row3 span1").click(function(){
-      if ($("#row3 span1")[0].innerHTML == "+") {
-        $("#row3 span1")[0].innerHTML = players[counter % 2];
-        push_to_state();
-      } else {
-        counter = counter - 1;
-      };
-    });
-  });
-  $(document).ready(function(){
-    $("#row3 span2").click(function(){
-      if ($("#row3 span2")[0].innerHTML == "+") {
-        $("#row3 span2")[0].innerHTML = players[counter % 2];
-        push_to_state();
-      } else {
-        counter = counter - 1;
-      };
-    });
-  });
 }
