2e012aa7018e2d1951ac2ad9696e49e7b4a2ec08
John Moon <johnmoonyy@gmail.com>
2014-03-13 08:33:29 -0400

    initial set up for Jasmine test

---
+++ spec/javascripts/boardSpec.js
@@ -0,0 +1,8 @@
+describe('Board function', function() {
+  it('creates a board on the UI', function() {
+    expect(('.row')).toEqual('span')
+    //expect(todos.model).toEqual(Todo.models.Todo);
+  });
+});
+
+
